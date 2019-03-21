fdd041f56559b73ae143657a0afac200bfa40b8c
Aaron M. Griffin <uurrnn@gmail.com>
2019-03-21 15:19:54 -0400

    added variables file and weclome styles

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -2,8 +2,8 @@ import { h } from "preact";
 
 const Welcome = () => (
     <aside className="welcome">
-        <h1>gov game</h1>
-        <p>a government simulation game based on the u.s. government</p>
+        <h1>Gov g ame</h1>
+        <p>A government simulation game based on the u.s. government</p>
     </aside>
 );
 
