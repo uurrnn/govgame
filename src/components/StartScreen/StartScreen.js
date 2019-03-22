132f412678237c34fabbbc77587b31c8c015a0a7
Aaron M. Griffin <uurrnn@gmail.com>
2019-03-22 17:44:36 -0400

    added bg image to start screen

--- src/components/StartScreen/StartScreen.js
+++ src/components/StartScreen/StartScreen.js
@@ -1,9 +1,12 @@
 import { h, render, Component } from "preact";
 
+import WH from "../../assets/white-house.jpg";
+
 class StartScreen extends Component {
     render() {
         return (
             <div class="startScreen">
+                <img src={WH} />
                 <h1>Seifu</h1>
                 <menu>
                     <button class="newGame">New Game</button>
